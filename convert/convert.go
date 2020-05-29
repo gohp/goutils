2984bef881d0f62cb5056272e9e3e0b9aa2afb24
jam <jamwong994@gmail.com>
2020-05-29 18:35:35 +0800

    convert utils: init

---
+++ convert/convert.go
@@ -0,0 +1,17 @@
+package convert
+
+func StringToInt()  {
+	panic("i")
+}
+
+func StringToInt64()  {
+	panic("i")
+}
+
+func IntToString()  {
+	panic("i")
+}
+
+func Int64ToString()  {
+	panic("i")
+}
---
+++ convert/convert_test.go
@@ -0,0 +1,19 @@
+package convert
+
+import "testing"
+
+func TestInt64ToString(t *testing.T) {
+
+}
+
+func TestIntToString(t *testing.T) {
+
+}
+
+func TestStringToInt(t *testing.T) {
+
+}
+
+func TestStringToInt64(t *testing.T) {
+
+}
