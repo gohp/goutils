302f48de37c15af127edc3bd37e9385d4be25339
jam <jamwong994@gmail.com>
2020-05-31 22:06:48 +0800

    update gotime

--- gotime/gotime.go
+++ gotime/gotime.go
@@ -6,6 +6,15 @@ import (
 	"time"
 )
 
+type Gotime struct {
+	time.Time
+}
+
+func (g Gotime) MarshalJSON() ([]byte, error){
+	stamp := fmt.Sprintf("\"%s\"", FormatDatetime(g, "YYYY-MM-DD HH:mm:ss"))
+	return []byte(stamp), nil
+}
+
 // Format time.Time struct to string
 // MM - month - 01
 // M - month - 1, single bit
@@ -38,4 +47,30 @@ func FormatDatetime(t time.Time, format string) string {
 	res = strings.Replace(res, "ss", t.Format("05"), -1)
 	res = strings.Replace(res, "s", t.Format("5"), -1)
 	return res
+}
+
+func Now() string {
+	return FormatDatetime(time.Now(), "YYYY-MM-DD HH:mm:ss")
+}
+
+func NowUnix() int64 {
+	return time.Now().Unix()
+}
+
+// TodayStart eg: 2018-01-01 00:00:00
+func TodayStart() string {
+	now := time.Now()
+	tm := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0,  time.Local)
+	return FormatDatetime(tm, "YYYY-MM-DD HH:mm:ss")
+}
+
+// TodayEnd eg: 2018-01-01 23:59:59
+func TodayEnd() string {
+	now := time.Now()
+	tm := time.Date(now.Year(), now.Month(), now.Day(), 23, 59, 59, 1e9-1,  time.Local)
+	return FormatDatetime(tm, "YYYY-MM-DD HH:mm:ss")
+}
+
+func ToUnix() int64 {
+	panic("1")
 }
