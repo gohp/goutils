ff0b95af359f4f5af05962ecca648983866551a9
jam <jamwong994@gmail.com>
2020-06-01 12:04:12 +0800

    update jwt unit test

--- jwt/jwt_test.go
+++ jwt/jwt_test.go
@@ -1,1 +1,21 @@
 package jwt
+
+import (
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+	"time"
+)
+
+func TestTokenBucket_TakeOne(t *testing.T) {
+	Convey("test jwt", t, func() {
+		j := New(&Config{
+			Key:        "1234",
+		})
+
+		token, err := j.EncodeToken("uid_12345", time.Now())
+		So(err, ShouldBeNil)
+		obj, err := j.DecodeToken(token)
+		So(err, ShouldBeNil)
+		So(obj.ID, ShouldEqual, "uid_12345")
+	})
+}
