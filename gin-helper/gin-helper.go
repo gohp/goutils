8d9364912d5a37b0af43dae1ec73b2b87aa0435e
jam <jamwong994@gmail.com>
2020-06-07 19:55:18 +0800

    maybe have a gin helper

---
+++ gin-helper/gin-helper.go
@@ -0,0 +1,39 @@
+package gin_helper
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+/**
+* @Author: Jam Wong
+* @Date: 2020/6/7
+ */
+
+type ErrResponse struct {
+	Success bool   `json:"success"`
+	Code    int    `json:"error_code"`
+	Message string `json:"error_message"`
+}
+
+type OkResponse struct {
+	Success bool        `json:"success"`
+	Result  interface{} `json:"result"`
+}
+
+func WriteResponse(c *gin.Context, payload interface{}) {
+	// always return http.StatusOK
+	c.JSON(http.StatusOK, OkResponse{
+		Success: true,
+		Result:  payload,
+	})
+}
+
+func WriteError(c *gin.Context, errCode int, errMsg string) {
+	code := http.StatusOK
+	c.JSON(code, ErrResponse{
+		Success: false,
+		Code:    errCode,
+		Message: errMsg,
+	})
+}
