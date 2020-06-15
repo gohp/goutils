772b4d0ef1e422cbbc99238702485fb99e8bda2b
jam <jamwong994@gmail.com>
2020-06-15 11:29:29 +0800

    add httplib

---
+++ httplib/httplib_test.go
@@ -0,0 +1,29 @@
+package httplib
+
+import (
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+)
+
+/**
+* @Author: Jam Wong
+* @Date: 2020/6/15
+ */
+
+func TestGet(t *testing.T) {
+	Convey("Test Http Get", t, func() {
+		So(nil, ShouldEqual, nil)
+	})
+}
+
+func TestPost(t *testing.T) {
+	Convey("Test Http Get", t, func() {
+		So(nil, ShouldEqual, nil)
+	})
+}
+
+func TestDelete(t *testing.T) {
+	Convey("Test Http Get", t, func() {
+		So(nil, ShouldEqual, nil)
+	})
+}
