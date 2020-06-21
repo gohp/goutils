4bd0657d09fc2457570c69f2b39eadff7ebd1f2f
jam <jamwong994@gmail.com>
2020-06-21 15:03:46 +0800

    add sort

--- sort/sort.go
+++ sort/sort.go
@@ -5,5 +5,29 @@ package sort
 * @Date: 2020/6/21
  */
 
+// 冒泡排序
+func Bubble()  {
 
- 
+}
+
+// 选择排序
+func Select()  {
+
+}
+
+// 插入排序
+func Insert()  {
+
+}
+
+// 归并排序
+func Merge()  {
+
+}
+
+// 快速排序
+func Quick()  {
+
+}
+
+// 堆排序
