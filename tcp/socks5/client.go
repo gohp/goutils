39220b0e927f966d23ef358c9b5e20c163794b82
jam <jamwong994@gmail.com>
2020-07-21 17:50:30 +0800

    add tcp-pr:init

--- tcp/socks5/client.go
+++ tcp/socks5/client.go
@@ -1,17 +1,56 @@
 package socks5
 
+import (
+	"log"
+	"net"
+)
+
 /**
 * @Author: Jam Wong
 * @Date: 2020/7/21
  */
 
 type TcpClient struct {
+	listenAddr string
+	serverAddr string
 }
 
-func NewClient() *TcpClient {
-	return &TcpClient{}
+func NewClient(listenAddr string, serverAddr string) *TcpClient {
+	c := &TcpClient{
+		listenAddr: listenAddr,
+		serverAddr: serverAddr,
+	}
+	return c
 }
 
 func (client *TcpClient) Run() {
+	// proxy地址
+	serverAddr, err := net.ResolveTCPAddr("tcp", client.serverAddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("connect to remote server: %s ....", client.serverAddr)
+
+	listenAddr, err := net.ResolveTCPAddr("tcp", client.listenAddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("listen on: %s ", client.listenAddr)
+
+	listener, err := net.ListenTCP("tcp", listenAddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		localClient, err := listener.AcceptTCP()
+		if err != nil {
+			log.Fatal(err)
+		}
+		go client.handleRequest(localClient, serverAddr)
+	}
+}
+
+func (client *TcpClient) handleRequest(localClient *net.TCPConn, serverAddr *net.TCPAddr)  {
 	panic("no implement")
 }
--- tcp/socks5/server.go
+++ tcp/socks5/server.go
@@ -1,17 +1,49 @@
 package socks5
 
+import (
+	"log"
+	"net"
+)
+
 /**
 * @Author: Jam Wong
 * @Date: 2020/7/21
  */
 
 type TcpServer struct {
+	listenAddr string
 }
 
-func NewServer() *TcpServer {
-	return &TcpServer{}
+func NewServer(addr string) *TcpServer {
+	s := &TcpServer{
+		listenAddr: addr,
+	}
+	return s
 }
 
-func (client *TcpServer) Run() {
+func (server *TcpServer) Run() {
+	// listen client
+	listenAddr, err := net.ResolveTCPAddr("tcp", server.listenAddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("listen on: %s ", server.listenAddr)
+
+	listener, err := net.ListenTCP("tcp", listenAddr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer listener.Close()
+
+	for {
+		conn, err := listener.AcceptTCP()
+		if err != nil {
+			log.Fatal(err)
+		}
+		go server.handleRequest(conn)
+	}
+}
+
+func (server *TcpServer) handleRequest(conn *net.TCPConn)  {
 	panic("no implement")
 }
