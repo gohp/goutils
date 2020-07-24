9ebeaaeb50405f16129426370aa723f0ac84f5be
jam <jamwong994@gmail.com>
2020-07-24 14:40:43 +0800

    update convert

--- convert/convert.go
+++ convert/convert.go
@@ -1,6 +1,8 @@
 package convert
 
 import (
+	"fmt"
+	"reflect"
 	"strconv"
 )
 
@@ -24,58 +26,46 @@ func Int64ToString(i int64) string {
 	return strconv.FormatInt(i, 10)
 }
 
-//// ToInt64 convert any numeric value to int64
-//func toInt64(value interface{}) (d int64, err error) {
-//	val := reflect.ValueOf(value)
-//	switch value.(type) {
-//	case int, int8, int16, int32, int64:
-//		d = val.Int()
-//	case uint, uint8, uint16, uint32, uint64:
-//		d = int64(val.Uint())
-//	default:
-//		err = fmt.Errorf("ToInt64 need numeric not `%T`", value)
-//	}
-//	return
-//}
-//
-//// toString convert any value to string
-//func toString(value interface{}) (d string, err error) {
-//	val := reflect.ValueOf(value)
-//	switch value.(type) {
-//  case int:
-//		vs.Add(key, strconv.Itoa(rv))
-//	case int8:
-//		vs.Add(key, strconv.Itoa(int(rv)))
-//	case int16:
-//		vs.Add(key, strconv.Itoa(int(rv)))
-//	case int32:
-//		vs.Add(key, strconv.Itoa(int(rv)))
-//	case int64:
-//		vs.Add(key, strconv.FormatInt(rv, 10))
-//	case uint:
-//		vs.Add(key, strconv.FormatUint(uint64(rv), 10))
-//	case uint8:
-//		vs.Add(key, strconv.Itoa(int(rv)))
-//	case uint16:
-//		vs.Add(key, strconv.Itoa(int(rv)))
-//	case uint32:
-//		vs.Add(key, strconv.FormatUint(uint64(rv), 10))
-//	case uint64:
-//		vs.Add(key, strconv.FormatUint(rv, 10))
-//	case []byte:
-//		vs.Add(key, string(rv))
-//	case string:
-//		vs.Add(key, rv)
-//	case int:
-//		d = strconv.Itoa(int(val.Int()))
-//	case int64:
-//		d = strconv.FormatInt(val.Int(),10)
-//	case int8, int16, int32:
-//		d = val.Int()
-//	case uint, uint8, uint16, uint32, uint64:
-//		d = int64(val.Uint())
-//	default:
-//		err = fmt.Errorf("ToInt64 need numeric not `%T`", value)
-//	}
-//	return
-//}
+// ToInt64 convert any numeric value to int64
+func ToInt64(value interface{}) (d int64, err error) {
+	val := reflect.ValueOf(value)
+	switch value.(type) {
+	case int, int8, int16, int32, int64:
+		d = val.Int()
+	case uint, uint8, uint16, uint32, uint64:
+		d = int64(val.Uint())
+	default:
+		err = fmt.Errorf("ToInt64 need numeric not `%T`", value)
+	}
+	return
+}
+
+// toString convert any value to string
+func AnyToString(value interface{}) (d string, err error) {
+	val := reflect.ValueOf(value)
+	switch value.(type) {
+	case int:
+		d = strconv.Itoa(int(val.Int()))
+	case int64:
+		d = strconv.FormatInt(val.Int(), 10)
+	case int8, int16, int32:
+		d = strconv.Itoa(int(val.Int()))
+	case uint, uint32, uint64:
+		d = strconv.FormatUint(val.Uint(), 10)
+	case uint8, uint16:
+		d = strconv.Itoa(int(val.Int()))
+	case []byte:
+		d = string(val.Bytes())
+	case string:
+		d = val.String()
+	case bool:
+		if val.Bool() {
+			d = "true"
+		} else {
+			d = "false"
+		}
+	default:
+		err = fmt.Errorf("not `%T`", value)
+	}
+	return
+}
--- convert/convert_test.go
+++ convert/convert_test.go
@@ -32,3 +32,101 @@ func TestStringToInt64(t *testing.T) {
 		So(int64(123), ShouldEqual, i)
 	})
 }
+
+func TestToInt64(t *testing.T) {
+	Convey("Test ToInt64", t, func() {
+		Convey("Test int ToInt64", func() {
+			var i1 int = 1
+			var i2 int8 = 1
+			var i3 int16 = 1
+			var i4 int32 = 1
+			var i5 int64 = 1
+			i1r, err := ToInt64(i1)
+			So(i1r, ShouldEqual, i5)
+			So(err, ShouldBeNil)
+
+			i2r, err := ToInt64(i2)
+			So(i2r, ShouldEqual, i5)
+			So(err, ShouldBeNil)
+
+			i3r, err := ToInt64(i3)
+			So(i3r, ShouldEqual, i5)
+			So(err, ShouldBeNil)
+
+			i4r, err := ToInt64(i4)
+			So(i4r, ShouldEqual, i5)
+			So(err, ShouldBeNil)
+
+			i5r, err := ToInt64(i5)
+			So(i5r, ShouldEqual, i5)
+			So(err, ShouldBeNil)
+		})
+
+		Convey("Test uint ToInt64", func() {
+			var i int64 = 1
+
+			var u1 uint = 1
+			var u2 uint8 = 1
+			var u3 uint16 = 1
+			var u4 uint32 = 1
+			var u5 uint64 = 1
+			u1r, err := ToInt64(u1)
+			So(u1r, ShouldEqual, i)
+			So(err, ShouldBeNil)
+
+			u2r, err := ToInt64(u2)
+			So(u2r, ShouldEqual, i)
+			So(err, ShouldBeNil)
+
+			u3r, err := ToInt64(u3)
+			So(u3r, ShouldEqual, i)
+			So(err, ShouldBeNil)
+
+			u4r, err := ToInt64(u4)
+			So(u4r, ShouldEqual, i)
+			So(err, ShouldBeNil)
+
+			u5r, err := ToInt64(u5)
+			So(u5r, ShouldEqual, i)
+			So(err, ShouldBeNil)
+		})
+
+		Convey("Test non-numeric ToInt64", func() {
+			var u1 string = "1"
+			var u2 bool = true
+			_, err := ToInt64(u1)
+			So(err, ShouldNotBeNil)
+
+			_, err = ToInt64(u2)
+			So(err, ShouldNotBeNil)
+		})
+	})
+}
+
+func TestAnyToString(t *testing.T) {
+	Convey("Test AnyToString", t, func() {
+		res, err := AnyToString(1)
+		So(err, ShouldBeNil)
+		So(res, ShouldEqual, "1")
+
+		res, err = AnyToString(int64(1))
+		So(err, ShouldBeNil)
+		So(res, ShouldEqual, "1")
+
+		res, err = AnyToString(-1)
+		So(err, ShouldBeNil)
+		So(res, ShouldEqual, "-1")
+
+		res, err = AnyToString([]byte("1"))
+		So(err, ShouldBeNil)
+		So(res, ShouldEqual, "1")
+
+		res, err = AnyToString(true)
+		So(err, ShouldBeNil)
+		So(res, ShouldEqual, "true")
+
+		res, err = AnyToString(false)
+		So(err, ShouldBeNil)
+		So(res, ShouldEqual, "false")
+	})
+}
