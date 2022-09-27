9c7b014f649f7df54a1185531fb89fd842b49d6b
jam <jamwong994@gmail.com>
2022-09-27 17:11:43 +0800

    update gin helper: logging

--- gin-helper/middleware/logging/logging.go
+++ gin-helper/middleware/logging/logging.go
@@ -33,11 +33,6 @@ func Logging() gin.HandlerFunc {
 			return
 		}
 
-		// Skip for the health check requests.
-		if path == "/sd/health" || path == "/sd/ram" || path == "/sd/cpu" || path == "/sd/disk" {
-			return
-		}
-
 		// Read the Body content
 		var bodyBytes []byte
 		if c.Request.Body != nil {
@@ -74,37 +69,23 @@ func Logging() gin.HandlerFunc {
 		end := time.Now().UTC()
 		latency := end.Sub(start)
 		latencys := fmt.Sprintf("%s", latency)
-		uid := c.GetString("uid")
-		//responseBody := blw.body.Bytes()
+		responseBody := blw.body.Bytes()
 		// get code and message
 		var response gin_helper.BaseResponse
-		if err := json.Unmarshal(blw.body.Bytes(), &response); err != nil {
+		if err := json.Unmarshal(responseBody, &response); err != nil {
 			logger.Info(
 				"response body can not unmarshal to model.Response struct",
-				logger.With("uid", uid),
 				logger.With("trace", xRequests),
-				logger.With("ip", ip),
-				logger.With("method", method),
-				logger.With("path", path),
-				logger.With("requests_body", string(bodyBytes)),
-				logger.With("content_type", headers.Get("Content-Type")),
-				logger.With("user_agent", c.Request.UserAgent()),
+				logger.With("latency", latencys),
+				logger.With("response", responseBody),
 			)
 
 		} else {
 			logger.Info(
-				fmt.Sprintf("response success: %v", response.Success),
-				logger.With("uid", uid),
+				"response",
+				logger.With("success", response.Success),
 				logger.With("trace", xRequests),
-				logger.With("code", response.Code),
-				logger.With("message", response.Message),
-				logger.With("ip", ip),
 				logger.With("latency", latencys),
-				logger.With("method", method),
-				logger.With("path", path),
-				logger.With("requests_body", string(bodyBytes)),
-				logger.With("content_type", headers.Get("Content-Type")),
-				logger.With("user_agent", c.Request.UserAgent()),
 			)
 		}
 	}
@@ -113,18 +94,7 @@ func Logging() gin.HandlerFunc {
 func TraceLoggerMiddleware() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		// 每个请求生成的请求traceId具有全局唯一性
-		logger.AddContext(ctx, logger.With("trace", ctx.MustGet("X-Request-Id")))
-		// 为日志添加请求的地址以及请求参数等信息
-		logger.AddContext(ctx, logger.With("method", ctx.Request.Method))
-		logger.AddContext(ctx, logger.With("user_agent", ctx.Request.UserAgent()))
-		logger.AddContext(ctx, logger.With("path", ctx.Request.URL.String()))
-
-		// 将请求参数json序列化后添加进日志上下文
-		if ctx.Request.Form == nil {
-			_ = ctx.Request.ParseMultipartForm(32 << 20)
-		}
-		form, _ := json.Marshal(ctx.Request.Form)
-		logger.AddContext(ctx, logger.With("params", string(form)))
+		logger.GAddContext(ctx, logger.With("trace", ctx.MustGet("X-Request-Id")))
 		ctx.Next()
 	}
 }
--- go.mod
+++ go.mod
@@ -15,7 +15,7 @@ require (
 
 require (
 	github.com/BurntSushi/toml v1.2.0 // indirect
-	github.com/gohp/logger v0.2.4
+	github.com/gohp/logger v0.3.2
 	github.com/golang-jwt/jwt/v4 v4.4.2
 	github.com/lestrrat-go/file-rotatelogs v2.4.0+incompatible // indirect
 	github.com/lestrrat-go/strftime v1.0.6 // indirect
@@ -29,29 +29,29 @@ require (
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
-	github.com/go-playground/validator/v10 v10.10.0 // indirect
-	github.com/goccy/go-json v0.9.7 // indirect
+	github.com/go-playground/validator/v10 v10.11.1 // indirect
+	github.com/goccy/go-json v0.9.11 // indirect
 	github.com/golang/protobuf v1.5.0 // indirect
 	github.com/gopherjs/gopherjs v0.0.0-20181017120253-0766667cb4d1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/jtolds/gls v4.20.0+incompatible // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
-	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
-	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
+	github.com/pelletier/go-toml/v2 v2.0.5 // indirect
 	github.com/prometheus/client_model v0.2.0 // indirect
 	github.com/prometheus/common v0.10.0 // indirect
 	github.com/prometheus/procfs v0.1.3 // indirect
 	github.com/smartystreets/assertions v0.0.0-20180927180507-b2de0cb4f26d // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
-	go.uber.org/multierr v1.6.0 // indirect
-	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
-	golang.org/x/net v0.0.0-20211008194852-3b03d305991f // indirect
-	golang.org/x/sys v0.0.0-20211007075335-d3039528d8ac // indirect
-	google.golang.org/protobuf v1.28.0 // indirect
+	go.uber.org/multierr v1.8.0 // indirect
+	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3 // indirect
+	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2 // indirect
+	golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
 
